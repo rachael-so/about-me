9fe15ae3d1d85c0a8b6c77f2014c9ae360f8b7b0
Aaron McPeek <arn1313@comcast.net>
2017-06-08 14:13:24 -0700

    q7 and bugfix

--- app.js
+++ app.js
@@ -42,89 +42,94 @@ function question2(){
 }
 //Question 3
 function question3(){
-var answer3 = prompt('I have a dog named Happy!').toUpperCase();
-if (answer3 === 'YES' || answer3 === 'Y') {
-  alert('Correct! My dad got Happy for me when I was in 3rd grade after I persistently asked for a dog.');
-  correctCount++;
-  console.log('user responded "YES" to question 3: Correct');
-} else if (answer3 === 'NO' || answer3 === 'N') {
-  alert('Incorrect! My dad got Happy for me when I was in 3rd grade after I persistently asked for a dog.');
-  console.log('user responded "NO" to question 3: Incorrect');
-} else {
-  alert('ERROR! Make sure to enter either "YES" or "NO"');
-}
+  var answer3 = prompt('I have a dog named Happy!').toUpperCase();
+  if (answer3 === 'YES' || answer3 === 'Y') {
+    alert('Correct! My dad got Happy for me when I was in 3rd grade after I persistently asked for a dog.');
+    correctCount++;
+    console.log('user responded "YES" to question 3: Correct');
+  } else if (answer3 === 'NO' || answer3 === 'N') {
+    alert('Incorrect! My dad got Happy for me when I was in 3rd grade after I persistently asked for a dog.');
+    console.log('user responded "NO" to question 3: Incorrect');
+  } else {
+    alert('ERROR! Make sure to enter either "YES" or "NO"');
+  }
 }
 //Question 4
 function question4(){
-var answer4 = prompt('I have visited Japan, Australia, France, and Italy').toUpperCase();
-if (answer4 === 'YES' || answer4 === 'Y') {
-  alert('Incorrect! These are all places I really want to visit.');
-  console.log('user responded "YES" to question 4: Incorrect');
-} else if (answer4 === 'NO' || answer4 === 'N') {
-  alert('Correct! These are all places I really want to visit.');
-  correctCount++;
-  console.log('user responded "NO" to question 4: Correct');
-} else {
-  alert('ERROR! Make sure to enter either "YES" or "NO"');
-}
+  var answer4 = prompt('I have visited Japan, Australia, France, and Italy').toUpperCase();
+  if (answer4 === 'YES' || answer4 === 'Y') {
+    alert('Incorrect! These are all places I really want to visit.');
+    console.log('user responded "YES" to question 4: Incorrect');
+  } else if (answer4 === 'NO' || answer4 === 'N') {
+    alert('Correct! These are all places I really want to visit.');
+    correctCount++;
+    console.log('user responded "NO" to question 4: Correct');
+  } else {
+    alert('ERROR! Make sure to enter either "YES" or "NO"');
+  }
 }
 //Question 5
 function question5(){
-var answer5 = prompt('I am very excited to work in computer science one day!').toUpperCase();
-if (answer5 === 'YES' || answer5 === 'Y') {
-  alert('Correct! I really want to become a good coder and use my skills to do something useful.');
-  correctCount++;
-  console.log('user responded "YES" to question 5: Correct');
-} else if (answer5 === 'NO' || answer5 == 'N') {
-  alert('Incorrect! Of course I am excited! I really want to become a good coder and use my skills to do something useful.');
-  console.log('user responded "NO" to question 5: Incorrect');
-} else {
-  alert('ERROR! Make sure to enter either "YES" or "NO"');
+  var answer5 = prompt('I am very excited to work in computer science one day!').toUpperCase();
+  if (answer5 === 'YES' || answer5 === 'Y') {
+    alert('Correct! I really want to become a good coder and use my skills to do something useful.');
+    correctCount++;
+    console.log('user responded "YES" to question 5: Correct');
+  } else if (answer5 === 'NO' || answer5 == 'N') {
+    alert('Incorrect! Of course I am excited! I really want to become a good coder and use my skills to do something useful.');
+    console.log('user responded "NO" to question 5: Incorrect');
+  } else {
+    alert('ERROR! Make sure to enter either "YES" or "NO"');
+  }
 }
-}
-//part 2 of game
-alert('For the next two questions try to get the correct answers. You will be given an indicated number of tries.');
-
 //Question 6
 function question6(){
-for (var i = 0; i < 4; i++) {
-  var answer6 = prompt('Can you guess how old I am? *use integers (4 tries)').toUpperCase();
-  if (answer6 === '19') {
-    alert('Correct! My birth date is March 26, 1998.');
-    i = 4;
-    correctCount++;
-    console.log('user correctly responded "19" to question 6: Correct');
-  } else {
-    if (i === 3) {
-      alert('Incorrect! My birthdate is March 26, 1998');
+  for (var i = 0; i < 4; i++) {
+    var answer6 = prompt('Can you guess how old I am? *use integers (4 tries)').toUpperCase();
+    if (answer6 === '19') {
+      alert('Correct! My birth date is March 26, 1998.');
+      i = 4;
+      correctCount++;
+      console.log('user correctly responded "19" to question 6: Correct');
     } else {
-      if (answer6 < 19)
-      alert('Your guess is too low!');
-      if (answer6 > 19)
-      alert('Your guess is too high!');
-    }
-    console.log('user responded "' + answer7 + '" to question 6: Incorrect');
-  } //end big if/else statment
-} //end for loop
+      if (i === 3) {
+        alert('Incorrect! My birthdate is March 26, 1998');
+      } else {
+        if (answer6 < 19)
+          alert('Your guess is too low!');
+        if (answer6 > 19)
+          alert('Your guess is too high!');
+      }
+      console.log('user responded "' + answer6 + '" to question 6: Incorrect');
+    } //end big if/else statment
+  } //end for loop
 }
 //Question 7
 function question7(){
-for (var i = 0; i < 6; i++) {
-  var answer7 = prompt('Can you guess the breed of one of the dogs I have owned?').toUpperCase();
-  if (answer7 === 'YELLOW LABRADOR RETRIEVER' || answer7 === 'LABRADOR RETRIEVER' || answer7 === 'LABRADOR' || answer7 === 'RETRIEVER' || answer7 === 'LAB' || answer7 === 'BICHON FRISE' || answer7 === 'BICHON' || answer7 === 'MALTESE') {
-    alert('Correct! I have had two dogs: a yellow labrador retriever and a bichon frise, maltese mix.');
-    i = 6;
-    correctCount++;
-    console.log('user responded "' + answer7 + '" to question 7: Correct');
-  } else {
-    if (i === 5) {
-      alert('Incorrect! I have had two dogs: a yellow labrador retriever and a bichon frise, maltese mix.');
+  for (var i = 0; i < 6; i++) {
+    var answer7 = prompt('Can you guess the breed of one of the dogs I have owned?').toUpperCase();
+    if (answer7 === 'YELLOW LABRADOR RETRIEVER' || answer7 === 'LABRADOR RETRIEVER' || answer7 === 'LABRADOR' || answer7 === 'RETRIEVER' || answer7 === 'LAB' || answer7 === 'BICHON FRISE' || answer7 === 'BICHON' || answer7 === 'MALTESE') {
+      alert('Correct! I have had two dogs: a yellow labrador retriever and a bichon frise, maltese mix.');
+      i = 6;
+      correctCount++;
+      console.log('user responded "' + answer7 + '" to question 7: Correct');
     } else {
-      alert('Incorrect! try again');
-    }
-    console.log('user incorrectly responded "' + answer7 + '" to question 7: Correct');
-  } //end big if/else statement
-} //end for loop
+      if (i === 5) {
+        alert('Incorrect! I have had two dogs: a yellow labrador retriever and a bichon frise, maltese mix.');
+      } else {
+        alert('Incorrect! try again');
+      }
+      console.log('user incorrectly responded "' + answer7 + '" to question 7: Correct');
+    } //end big if/else statement
+  } //end for loop
 }
+question1();
+question2();
+question3();
+question4();
+question5();
+alert('For the next two questions try to get the correct answers. You will be given an indicated number of tries.');
+question6();
+question7();
 //THE END
 alert('You have completed the quiz. Your correctly answered ' + correctCount + ' out of 7 questions. Thank you, ' + username + ', for getting to know me! To learn more, continue reading this page.');
